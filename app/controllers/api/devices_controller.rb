9ea676ccfff6ac3358140274a867c2a9b5345c01
teapuddles <kingsley.gleeson@gmail.com>
2020-12-12 17:47:34 -0500

    formatted phone_numbers

--- app/controllers/api/devices_controller.rb
+++ app/controllers/api/devices_controller.rb
@@ -1,5 +1,6 @@
 class Api::DevicesController < ApplicationController
     before_action :find_device, only: [:terminate, :create_heartbeat, :create_report]
+    
 
 # show devices (not needed)
 def all
@@ -20,35 +21,47 @@ end
 
 # update disabled_at
 def terminate
-    @device.disabled_at = DateTime.now
+    if @device.id 
+       @device.disabled_at = DateTime.now
 
-    @device.update(disable_device_params)
+       @device.update(disable_device_params)
 
-    render json: @device.to_json, status: :accepted
+       render json: @device.to_json, status: :accepted
+    else
+       render json: { error: "Invalid User", status: 500 }
+    end
 end 
 
 # create heartbeat only if device has nil for disabled_at
 def create_heartbeat
-    if  @device.disabled_at.nil?
+    if  @device.disabled_at.nil? && @device.id
         @device.heartbeats.create(heartbeat_params)
 
-        render json: @device.to_json, status: :created
+        render json: @device.heartbeats.to_json, status: :created
     else
-        render json: { error: "invalid user", status: 500 }
+        render json: { error: "Invalid User", status: 500 }
     end
 end
 
 # create report
 def create_report
-    @device.reports.create(report_params)
+    if  @device.disabled_at.nil?
+        @device.reports.create(report_params)
 
-    render json: @device.to_json, status: :created
+        render json: @device.reports.to_json, status: :created
+    else
+        render json: { error: "Invalid User", status: 500 }
+    end
 end 
 
 private
 
 def find_device
     @device = Device.find(params[:device_id])
+end
+
+def phone_number_format
+    phone_number.phone.e164
 end
 
 def device_params
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,4 +1,14 @@
 class Device < ApplicationRecord
     has_many :heartbeats
     has_many :reports
+
+    before_save :format_phone
+    validates :phone_number, presence: true, allow_blank: false
+
+    private
+
+    def format_phone
+        self.phone_number = Phonelib.parse(phone_number).e164
+    end
+
 end
